1703e6dd1e0e8a11728f2ee2064f015eaab21278
Kanad2004 <kanadkulkarni2013@gmail.com>
2025-01-19 14:13:20 +0530

    Merge conflict

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,45 +1,44 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {ApiError} from "../utils/ApiError.js"
-import {Admin} from "../model/Admin.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import jwt from "jsonwebtoken"
-import mongoose from "mongoose"
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { Admin } from "../model/Admin.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
-const login = asyncHandler (async (req,res) => {
+const login = asyncHandler(async (req, res) => {
+  const { adminEmail, adminPassword } = req.body;
 
-    const {adminEmail , adminPassword} = req.body
+  if (!adminEmail) {
+    throw new ApiError(400, "Email is required");
+  }
 
-    if(!adminEmail){
-        throw new ApiError(400 , "Email is required")
-    }
+  const admin = await Admin.findOne({ adminEmail });
 
-    const admin = await Admin.findOne({adminEmail})
+  if (!admin) {
+    throw new ApiError(404, "Admin not Found");
+  }
 
-    if(!admin){
-        throw new ApiError(404 , "Admin not Found")
-    }
+  const isPassValid = await admin.isPasswordCorrect(adminPassword);
 
-    const isPassValid = await admin.isPasswordCorrect(adminPassword)
+  if (!isPassValid) {
+    throw new ApiError(401, "Password is not valid");
+  }
 
-    if(!isPassValid){
-        throw new ApiError(401 , "Password is not valid")
-    }
-    
-    const newAdmin = await Admin.create({
-        adminName,
-        adminEmail, 
-        adminPassword ,
-    })
-    const createdUser = await User.findById(newUser._id).select(
-        "-adminPassword -refreshToken"
-    )
-    if(!createdUser){
-        throw new ApiError(500 , "User is not registered successfully")
-    }
+  const newAdmin = await Admin.create({
+    adminName,
+    adminEmail,
+    adminPassword,
+  });
+  const createdUser = await User.findById(newUser._id).select(
+    "-adminPassword -refreshToken"
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "User is not registered successfully");
+  }
 
-    return res.status(201).json(
-        new ApiResponse(200, createdUser, "User Registered Successfully ! ")
-    )
-})
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User Registered Successfully ! "));
+});
 
-export {login , logoutAdmin}
+export { login, logoutAdmin };
