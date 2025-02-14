081a4cad1cb07c08ac43704e47043534212dbe4c
Kanad2004 <kanadkulkarni2013@gmail.com>
2025-02-14 23:53:06 +0530

    Done

--- routes/newofferRouter.js
+++ routes/newofferRouter.js
@@ -17,10 +17,32 @@ import { upload } from "../middlewares/uploadFile.middleware.js";
 // Add Authentication and authorization middleware in this . . .
 const newofferRouter = express.Router();
 
-newofferRouter.post("/add-new-offer", authenticateAdmin,authorizeRoleAdmin(['Admin']) ,upload.single("link") , addNewOffer);
-newofferRouter.post("/edit-offer/:id", authenticateAdmin,authorizeRoleAdmin(['Admin']) , upload.single("link") , editNewOffer);
-newofferRouter.post("/delete-offer/:id", authenticateAdmin, authorizeRoleAdmin(['Admin']), deleteNewOffer);
+newofferRouter.post(
+  "/add-new-offer",
+  authenticateAdmin,
+  authorizeRoleAdmin(["Admin"]),
+  upload.single("link"),
+  addNewOffer
+);
+newofferRouter.post(
+  "/edit-offer/:id",
+  authenticateAdmin,
+  authorizeRoleAdmin(["Admin"]),
+  upload.single("link"),
+  editNewOffer
+);
+newofferRouter.post(
+  "/delete-offer/:id",
+  authenticateAdmin,
+  authorizeRoleAdmin(["Admin"]),
+  deleteNewOffer
+);
 // newofferRouter.post("/update-offer-img" , upload.single("link") , updateOfferImage)
-newofferRouter.get("/get-all-offers" ,authenticateAdmin,authorizeRoleAdmin(['Admin']) ,  getAllOffers)
+newofferRouter.get(
+  "/get-all-offers",
+  authenticateAdmin,
+  authorizeRoleAdmin(["Admin"]),
+  getAllOffers
+);
 
 export default newofferRouter;
