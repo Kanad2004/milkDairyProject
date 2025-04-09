b33f9aeb5f80c3088e0d979bd89828e16c4c2aca
Shreyash Ghanekar <shreyasghanekar35@gmail.com>
2025-04-09 16:47:30 +0530

    Added controller for report generation of all farmers loan and transaction

--- routes/transactionRouter.js
+++ routes/transactionRouter.js
@@ -11,7 +11,7 @@ import {
   generateFarmerReport
 } from "../controllers/transactionController.js";
 
-import { farmerTransaction } from "../controllers/farmerTrasactionController.js";
+import { farmerTransaction, downloadAllFarmersPDF } from "../controllers/farmerTrasactionController.js";
 
 import {
   authenticateAdmin,
@@ -127,6 +127,11 @@ transactionRouter.get(
   "/download-pdf/:mobileNumber",
 
   farmerTransaction
+);
+
+transactionRouter.get(
+  "/download-all-farmers-pdf",
+  downloadAllFarmersPDF
 );
 
 
